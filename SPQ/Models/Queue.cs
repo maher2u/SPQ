b78529fe4ee38f39b505e879d1fd906614117fb3
Maher <maher2u@hotmail.com>
2017-06-21 09:29:29 +0200

    mm

---
+++ SPQ/Models/Queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPQ.Models
+{
+    public class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+        }
+    }
+}
